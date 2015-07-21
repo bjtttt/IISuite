f2f025e26d31047f9611b7c5f1401325ba83c5cf
bjtttt <guozhitaobj@163.com>
2015-07-21 21:39:23 +0800

    Socket & Log
    NA

---
+++ app/src/main/java/com/keysight/guozhitao/iisuite/helper/LogService.java
@@ -0,0 +1,7 @@
+package com.keysight.guozhitao.iisuite.helper;
+
+/**
+ * Created by cn569363 on 7/21/2015.
+ */
+public class LogService {
+}
--- app/src/main/java/com/keysight/guozhitao/iisuite/helper/SocketService.java
+++ app/src/main/java/com/keysight/guozhitao/iisuite/helper/SocketService.java
@@ -17,23 +17,43 @@ public class SocketService implements Serializable {
         mGlobalSettings = globalSettings;
     }
 
-    public void OpenSocket() throws IOException {
+    private void OpenSocket() throws IOException {
         CloseSocket();
 
         mSocket = new Socket(mGlobalSettings.getCurrentServerInfo().getServer(), GlobalSettings.SOCKET_PORT);
     }
 
-    public void CloseSocket() throws IOException {
+    public void SafeOpenSocket() {
+        try {
+            OpenSocket();
+        }
+        catch(Exception e) {
+            mSocket = null;
+        }
+    }
+
+    private void CloseSocket() throws IOException {
         if(!mSocket.isClosed()) {
             mSocket.close();
             mSocket = null;
         }
     }
 
-    public void SendData(byte[] ba) throws IOException {
+    private void SendData(String s) throws IOException {
+        if(s == null)
+            throw new IllegalArgumentException("SocketService::SendData(null)");
+        if(s.isEmpty())
+            throw new IllegalArgumentException("SocketService::SendData(\"\")");
+
+        byte[] ba = s.getBytes();
+        SendData(ba);
+    }
+
+    private void SendData(byte[] ba) throws IOException {
         if(mSocket == null)
             OpenSocket();
-        
+
+
         if(!mGlobalSettings.getCurrentInstrumentInfo().getConnected())
             CloseSocket();
     }
